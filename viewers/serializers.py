7bfd9b7052f5e4119aa4b3e19f3c7a14db9270bf
hjasiel1234567890 <jasielsalvadorhdz@gmail.com>
2025-02-17 15:38:50 -0600

    serializer viewers

--- viewers/serializers.py
+++ viewers/serializers.py
@@ -1,11 +1,35 @@
 from rest_framework import serializers
+from datetime import datetime
+from collections import defaultdict
 from .models import Viewer
 
-# Create your views here.
+# Serializer for the viewer records
 class ViewersSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Viewer
-            fields = [
+    # Method to round the time to the desired format
+    def round_time(self, time_str):
+        # Convert time from "HH:MM:SS" format to a datetime object
+        try:
+            time_obj = datetime.strptime(time_str, '%H:%M:%S')
+            # Round minutes and seconds to zero
+            rounded_time = time_obj.replace(minute=0, second=0)
+            return rounded_time.strftime('%H:%M:%S')
+        except ValueError:
+            return time_str
+
+    # Override the representation method to add the rounded time field
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        # Add a new field 'period_start_time_rounded'
+        period_start_time = representation.get('period_start_time', '')
+        if period_start_time:
+            representation['period_start_time_rounded'] = self.round_time(period_start_time)
+
+        return representation
+
+    class Meta:
+        model = Viewer
+        fields = [
             'location_id',
             'period_start',
             'period_start_date',
@@ -19,5 +43,46 @@ class ViewersSerializer(serializers.ModelSerializer):
             'age',
             'dwell_time_in_tenths_of_sec',
             'attention_time_in_tenths_of_sec',
-            'age_value'
+            'age_value',
         ]
+
+# Serializer for the viewer summary
+class ViewersSummarySerializer(serializers.Serializer):
+    avg_age = serializers.FloatField()
+    avg_dwell_time_in_seconds = serializers.FloatField()
+    avg_attention_time_in_seconds = serializers.FloatField()
+    total_viewers = serializers.IntegerField()
+    total_male = serializers.IntegerField()
+    total_female = serializers.IntegerField()
+    very_happy = serializers.IntegerField()
+    happy = serializers.IntegerField()
+    neutral = serializers.IntegerField()
+    unhappy = serializers.IntegerField()
+    very_unhappy = serializers.IntegerField()
+    viewers = ViewersSerializer(many=True)
+
+    # Override the representation method to group viewers by rounded hour
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        # Use a dictionary to group viewers by rounded hour
+        grouped_by_hour = defaultdict(int)
+
+        # Group 'viewers' by their rounded hour and count the total for each hour
+        for viewer in representation.get('viewers', []):
+            period_start_time_rounded = viewer.get('period_start_time_rounded', '')
+            if period_start_time_rounded:
+                grouped_by_hour[period_start_time_rounded] += 1
+
+        # Create a list of results showing the total viewers for each rounded hour
+        viewers_by_hour = []
+        for hour, total_viewers in grouped_by_hour.items():
+            viewers_by_hour.append({
+                'hour': hour,
+                'total_viewers': total_viewers
+            })
+
+        # Replace the original list of viewers with the list grouped by hour
+        representation['viewers'] = viewers_by_hour
+
+        return representation
--- viewers/views.py
+++ viewers/views.py
@@ -6,7 +6,7 @@ from .models import Viewer
 from django.db.models import Sum, Avg
 from .serializers import ViewersSerializer
 from builtins import round
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 class ViewersDayView(APIView):
     def post(self, request):
@@ -35,25 +35,37 @@ class ViewersDayView(APIView):
             period_start_date__lte=end_date     # Filter until the end date
         )
         
-
         # Perform aggregations for the required fields
         avg_age = viewer_records.aggregate(Avg('age_value'))['age_value__avg'] or 0
         avg_dwell_time = viewer_records.aggregate(Avg('dwell_time_in_tenths_of_sec'))['dwell_time_in_tenths_of_sec__avg'] or 0
         avg_attention_time = viewer_records.aggregate(Avg('attention_time_in_tenths_of_sec'))['attention_time_in_tenths_of_sec__avg'] or 0
         
-        # emotions
-        # very_happy_percentage = (viewer_records.aggregate(Sum('very_happy'))['very_happy__sum'] or 0) / total_viewers * 100
-        # happy_percentage = (viewer_records.aggregate(Sum('happy'))['happy__sum'] or 0) / total_viewers * 100
-        # neutral_percentage = (viewer_records.aggregate(Sum('neutral'))['neutral__sum'] or 0) / total_viewers * 100
-        # unhappy_percentage = (viewer_records.aggregate(Sum('unhappy'))['unhappy__sum'] or 0) / total_viewers * 100
-        # very_unhappy_percentage = (viewer_records.aggregate(Sum('very_unhappy'))['very_unhappy__sum'] or 0) / total_viewers * 100
+        # Count emotions
+        emotion_counts = Counter({
+            'very_happy': 0,
+            'happy': 0,
+            'neutral': 0,
+            'unhappy': 0,
+            'very_unhappy': 0
+        })
+
+        # Iterate over the Viewer records to count the emotions
+        for viewer in viewer_records:
+            emotions = {
+                'very_happy': viewer.very_happy,
+                'happy': viewer.happy,
+                'neutral': viewer.neutral,
+                'unhappy': viewer.unhappy,
+                'very_unhappy': viewer.very_unhappy
+            }
+            # Identificar la emoción más alta
+            predominant_emotion = max(emotions, key=emotions.get)
+            emotion_counts[predominant_emotion] += 1
         
         # Total count
         total_male = viewer_records.filter(gender=1).count()
         total_female = viewer_records.filter(gender=2).count()
-        
-        
-        print(viewer_records)
+
         # Round the average values to 1 decimal place
         avg_age = round(avg_age, 1)
         avg_dwell_time = round(avg_dwell_time / 10, 1)  # Convert to seconds
@@ -61,7 +73,23 @@ class ViewersDayView(APIView):
 
         # Total number of Viewer records within the date range
         total_viewers = viewer_records.count()
-        print("avg_age", avg_age)
+
+        # Agrupar los viewers por hora redondeada
+        viewers_by_hour = defaultdict(int)
+
+        for viewer in viewer_records:
+            period_start_time = viewer.period_start_time
+            if period_start_time:
+                # Redondear la hora a 0 minutos y 0 segundos
+                rounded_hour = period_start_time.replace(minute=0, second=0).strftime('%H:%M:%S')
+                viewers_by_hour[rounded_hour] += 1
+
+        # Crear una lista con los resultados por hora
+        viewers_by_hour_list = [{
+            'hour': hour,
+            'total_viewers': total
+        } for hour, total in viewers_by_hour.items()]
+
         # Prepare the data for the response
         data = {
             'avg_age': avg_age,
@@ -69,9 +97,15 @@ class ViewersDayView(APIView):
             'avg_attention_time_in_seconds': avg_attention_time,
             'total_viewers': total_viewers,
             'total_male': total_male,
-            'total_female': total_female
+            'total_female': total_female,
+            'very_happy': emotion_counts['very_happy'],
+            'happy': emotion_counts['happy'],
+            'neutral': emotion_counts['neutral'],
+            'unhappy': emotion_counts['unhappy'],
+            'very_unhappy': emotion_counts['very_unhappy'],
+            'viewers': viewers_by_hour_list  # Solo los datos agrupados por hora
         }
-
+        
         # Return the response with the calculated averages
         return Response(data, status=status.HTTP_200_OK)
     
